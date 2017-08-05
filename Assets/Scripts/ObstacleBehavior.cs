4ce41b9d41934b879551cb73333b6dbbdd3484d1
lhj3883 <30744342+lhj3883@users.noreply.github.com>
2017-08-05 16:59:46 +0900

    first commit

---
+++ Assets/Scripts/ObstacleBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //this.gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+    }
+}
