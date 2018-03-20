e73f33fe60ca8429889d7367eaebff5697a6b29e
dg6676 <dg6676@gmail.com>
2018-03-20 23:59:10 +0900

    add gunsightBehavior basic
    add gunsight behavior basic

---
+++ Assets/Scripts/GunsightBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunsightBehavior : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void setMousePosition() {
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+    }
+}
