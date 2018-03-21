b65a5f9ca436b4eb0a0ddd64d31a1cb6359ab2dc
dg6676 <dg6676@gmail.com>
2018-03-22 01:29:39 +0900

    add gunsightBehavior and add gunsight gameobject.
    actually, it works by 'unity default mouse curser ( edit -
    projectSetting - player - cursor)' without gunsightBehavior.....

--- Assets/Scripts/GunsightBehavior.cs
+++ Assets/Scripts/GunsightBehavior.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class GunsightBehavior : MonoBehaviour {
 
-
-	// Use this for initialization
-	void Start () {
-		
+    public float speed;
+    // Use this for initialization
+    void Start () {
+        speed = 60f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float step = speed * Time.deltaTime;
+        Vector3 target = getMousePosition();
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
 	}
 
-    void setMousePosition() {
-        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+    Vector3 getMousePosition() {
+        Vector3 mousePosition = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y,0);
+        return mousePosition;
     }
 }
